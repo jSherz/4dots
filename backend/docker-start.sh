b35e9c660d12f9bec7a27df757876b2478c34c39
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2017-07-15 20:39:52 +0100

    Corrected binding and docker NODE_ENV.

--- backend/docker-start.sh
+++ backend/docker-start.sh
@@ -6,4 +6,5 @@ export DATABASE_URL="$(echo $MONGODB_PORT | sed "s/tcp/mongodb/g")/4dots"
 
 echo "Starting 4dots..."
 
+export NODE_ENV=production
 yarn run start
--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const GameSchema = require('./model/GameSchema');
 const environment = process.env.NODE_ENV || 'dev';
 
 const server = new Hapi.Server();
-server.connection({ port: 8080, host: 'localhost' });
+server.connection({ port: 8080, host: (environment === 'dev') ? 'localhost' : '0.0.0.0' });
 
 mongoose.connect(process.env.DATABASE_URL || 'mongodb://localhost/4dots', { useMongoClient: true });
 
