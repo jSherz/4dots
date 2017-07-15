ca3296a83f37e815b667baf3ead761b2ecced693
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2017-07-15 20:24:28 +0100

    Got basic service up and running.

---
+++ backend/docker-start.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "Setting DATABASE_URL..."
+
+export DATABASE_URL="$(echo $MONGODB_PORT | sed "s/tcp/mongodb/g")/4dots"
+
+echo "Starting 4dots..."
+
+yarn run start
--- infrastructure/app.tf
+++ infrastructure/app.tf
@@ -3,16 +3,32 @@
 resource "aws_ecs_task_definition" "4dots_app" {
   family = "4dots_app"
 
+  # This relies on the container's name being the same as the repository as the
+  # aws_ecr_repository.*.repository_url includes the name at the end.
+  # We could build this with the ECR ID & region but we can avoid doing so in
+  # this instance.
   container_definitions = <<DEFINITION
 [
   {
     "cpu": 128,
     "environment": [],
     "essential": true,
-    "image": "node:6.11",
+    "image": "mongo:3.2",
     "memory": 256,
     "memoryReservation": 128,
-    "name": "4dots_app"
+    "name": "mongodb"
+  },
+  {
+    "cpu": 128,
+    "environment": [],
+    "essential": true,
+    "image": "${aws_ecr_repository.4dots.repository_url}:latest",
+    "memory": 256,
+    "memoryReservation": 128,
+    "name": "4dots_app",
+    "links": [
+      "mongodb:mongodb"
+    ]
   }
 ]
 DEFINITION
