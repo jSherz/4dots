d8fdd1c92762ae6f0b0de029ada58f416544742b
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2017-07-16 11:48:05 +0100

    Corrected health check to point at leaderboard.

--- infrastructure/elb.tf
+++ infrastructure/elb.tf
@@ -61,7 +61,7 @@ resource "aws_elb" "4dots" {
     healthy_threshold   = 2
     unhealthy_threshold = 2
     timeout             = 3
-    target              = "HTTP:8080/"
+    target              = "HTTP:8080/leaderboard"
     interval            = 30
   }
 
